545d887c7378e59545e79155adaf6275c22e2b15
TRINH KY NAM <namct@TRINHs-MacBook-Pro.local>
2018-10-18 02:34:32 +0300

    fix issue when build: global undefined

--- app.js
+++ app.js
@@ -13,33 +13,33 @@ const socketio = require('./app/socket');
 app.use(cors());
 app.use(bodyParser.json());
 
-app.use(express.static(path.join(__dirname, 'dist/')));
+app.use(express.static(path.join(__dirname, 'dist/quiz-app')));
 
 // Create a database variable outside of the database connection callback to reuse the connection pool in your app.
 let db;
 
 mongodb.MongoClient.connect(
-    process.env.MONGODB_URI,
-    function (err, database) {
-        if (err) {
-            console.log(err);
-            process.exit(1);
-        }
-
-        // Save database object from the callback for reuse.
-        // mongo version ^3.0 will return a client object containing the database object
-        db = database.db('quiz-app');
-        console.log('Database connection ready');
-
-        // Initialize the app.
-        const port = process.env.PORT || 2112;
-        server.listen(port, () => {
-            console.log(`Quiz app listening on port ${port}`);
-        });
-
-        // start socket server
-        socketio.initSocketServer(server);
+  process.env.MONGODB_URI,
+  function (err, database) {
+    if (err) {
+      console.log(err);
+      process.exit(1);
     }
+
+    // Save database object from the callback for reuse.
+    // mongo version ^3.0 will return a client object containing the database object
+    db = database.db('quiz-app');
+    console.log('Database connection ready');
+
+    // Initialize the app.
+    const port = process.env.PORT || 2112;
+    server.listen(port, () => {
+      console.log(`Quiz app listening on port ${port}`);
+    });
+
+    // start socket server
+    socketio.initSocketServer(server);
+  }
 );
 
 const API = 'api';
@@ -51,161 +51,161 @@ const userCollection = 'USER_COLLECTION';
 
 // Generic error handler used by all endpoints.
 function handleError(res, reason, message, code) {
-    console.log(`ERROR: ${reason}`);
-    res.status(code || 500).json({
-        "error": message
-    });
+  console.log(`ERROR: ${reason}`);
+  res.status(code || 500).json({
+    "error": message
+  });
 };
 
 // This will allow Angular to handle the routing
 app.get('/', (req, res) => {
-    res.sendFile(path.resolve('dist/index.html'));
+  res.sendFile(path.resolve('dist/quiz-app/index.html'));
 });
 
 /* ===== Category API ===== */
 
 app.get(`/${API}/categories`, function (req, res) {
-    db.collection(categoryCollection).find({}).toArray(function (err, docs) {
-        if (err) {
-            handleError(res, err.message, "Failed.");
-        } else {
-            res.status(200).json(docs);
-        }
-    });
+  db.collection(categoryCollection).find({}).toArray(function (err, docs) {
+    if (err) {
+      handleError(res, err.message, "Failed.");
+    } else {
+      res.status(200).json(docs);
+    }
+  });
 });
 
 app.post(`/${API}/category`, function (req, res) {
-    let category = req.body;
-    category.createDate = new Date();
+  let category = req.body;
+  category.createDate = new Date();
 
-    if (!req.body.content) {
-        handleError(res, "Invalid request", 400);
-    } else {
-        db.collection(categoryCollection).insertOne(category, function (err, doc) {
-            if (err) {
-                handleError(res, err.message, "Failed");
-            } else {
-                res.status(201).json(doc.ops[0]);
-            }
-        });
-    }
+  if (!req.body.content) {
+    handleError(res, "Invalid request", 400);
+  } else {
+    db.collection(categoryCollection).insertOne(category, function (err, doc) {
+      if (err) {
+        handleError(res, err.message, "Failed");
+      } else {
+        res.status(201).json(doc.ops[0]);
+      }
+    });
+  }
 });
 
 
 /* ===== Question(s) API =====*/
 
 app.get(`/${API}/questions`, function (req, res) {
-    db.collection(questionCollection).find({}).toArray(function (err, docs) {
-        if (err) {
-            handleError(res, err.message, "Failed to get questions.");
-        } else {
-            res.status(200).json(docs);
-        }
-    });
+  db.collection(questionCollection).find({}).toArray(function (err, docs) {
+    if (err) {
+      handleError(res, err.message, "Failed to get questions.");
+    } else {
+      res.status(200).json(docs);
+    }
+  });
 });
 
 app.post(`/${API}/question`, function (req, res) {
-    let question = req.body;
-    question.createDate = new Date();
+  let question = req.body;
+  question.createDate = new Date();
 
-    if (!req.body.content) {
-        handleError(res, "Invalid request", 400);
-    } else {
-        db.collection(questionCollection).insertOne(question, function (err, doc) {
-            if (err) {
-                handleError(res, err.message, "Failed to create new question.");
-            } else {
-                res.status(201).json(doc.ops[0]);
-            }
-        });
-    }
+  if (!req.body.content) {
+    handleError(res, "Invalid request", 400);
+  } else {
+    db.collection(questionCollection).insertOne(question, function (err, doc) {
+      if (err) {
+        handleError(res, err.message, "Failed to create new question.");
+      } else {
+        res.status(201).json(doc.ops[0]);
+      }
+    });
+  }
 });
 
 
 /* ===== Answer API ===== */
 
 app.get(`/${API}/answer`, function (req, res) {
-    db.collection(answerCollection).find({}).toArray(function (err, docs) {
-        if (err) {
-            handleError(res, err.message, "Failed.");
-        } else {
-            res.status(200).json(docs);
-        }
-    });
+  db.collection(answerCollection).find({}).toArray(function (err, docs) {
+    if (err) {
+      handleError(res, err.message, "Failed.");
+    } else {
+      res.status(200).json(docs);
+    }
+  });
 });
 
 app.post(`/${API}/answer`, function (req, res) {
-    let answer = req.body;
-    answer.createDate = new Date();
+  let answer = req.body;
+  answer.createDate = new Date();
 
-    if (!req.body.content) {
-        handleError(res, "Invalid request", 400);
-    } else {
-        db.collection(answerCollection).insertOne(score, function (err, doc) {
-            if (err) {
-                handleError(res, err.message, "Failed");
-            } else {
-                res.status(201).json(doc.ops[0]);
-            }
-        });
-    }
+  if (!req.body.content) {
+    handleError(res, "Invalid request", 400);
+  } else {
+    db.collection(answerCollection).insertOne(score, function (err, doc) {
+      if (err) {
+        handleError(res, err.message, "Failed");
+      } else {
+        res.status(201).json(doc.ops[0]);
+      }
+    });
+  }
 });
 
 
 /* ===== Score API ===== */
 
 app.get(`/${API}/score`, function (req, res) {
-    db.collection(scoreCollection).find({}).toArray(function (err, docs) {
-        if (err) {
-            handleError(res, err.message, "Failed.");
-        } else {
-            res.status(200).json(docs);
-        }
-    });
+  db.collection(scoreCollection).find({}).toArray(function (err, docs) {
+    if (err) {
+      handleError(res, err.message, "Failed.");
+    } else {
+      res.status(200).json(docs);
+    }
+  });
 });
 
 app.post(`/${API}/score`, function (req, res) {
-    let score = req.body;
-    score.createDate = new Date();
+  let score = req.body;
+  score.createDate = new Date();
 
-    if (!req.body.content) {
-        handleError(res, "Invalid request", 400);
-    } else {
-        db.collection(scoreCollection).insertOne(score, function (err, doc) {
-            if (err) {
-                handleError(res, err.message, "Failed");
-            } else {
-                res.status(201).json(doc.ops[0]);
-            }
-        });
-    }
+  if (!req.body.content) {
+    handleError(res, "Invalid request", 400);
+  } else {
+    db.collection(scoreCollection).insertOne(score, function (err, doc) {
+      if (err) {
+        handleError(res, err.message, "Failed");
+      } else {
+        res.status(201).json(doc.ops[0]);
+      }
+    });
+  }
 });
 
 /* ===== User API ===== */
 
 app.get(`/${API}/user`, function (req, res) {
-    db.collection(userCollection).find({}).toArray(function (err, docs) {
-        if (err) {
-            handleError(res, err.message, "Failed.");
-        } else {
-            res.status(200).json(docs);
-        }
-    });
+  db.collection(userCollection).find({}).toArray(function (err, docs) {
+    if (err) {
+      handleError(res, err.message, "Failed.");
+    } else {
+      res.status(200).json(docs);
+    }
+  });
 });
 
 app.post(`/${API}/user`, function (req, res) {
-    let user = req.body;
-    user.createDate = new Date();
+  let user = req.body;
+  user.createDate = new Date();
 
-    if (!req.body.content) {
-        handleError(res, "Invalid request", 400);
-    } else {
-        db.collection(userCollection).insertOne(user, function (err, doc) {
-            if (err) {
-                handleError(res, err.message, "Failed");
-            } else {
-                res.status(201).json(doc.ops[0]);
-            }
-        });
-    }
+  if (!req.body.content) {
+    handleError(res, "Invalid request", 400);
+  } else {
+    db.collection(userCollection).insertOne(user, function (err, doc) {
+      if (err) {
+        handleError(res, err.message, "Failed");
+      } else {
+        res.status(201).json(doc.ops[0]);
+      }
+    });
+  }
 });
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,45 +1,43 @@
 import { Component } from '@angular/core';
 import * as socketIo from 'socket.io-client';
 import { ElementHelperService } from './element-helper.service';
+import * as $ from 'jquery';
 
 @Component({
-    selector: 'app-root',
-    templateUrl: './app.component.html',
-    styleUrls: ['./app.component.css']
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-    title = 'app works!';
+  title = 'app works!';
 
-    socket: SocketIOClient.Socket;
+  socket: SocketIOClient.Socket;
 
-    constructor(private elementHelper: ElementHelperService) {
-        socketIo();
-        this.socket = socketIo();
-        console.log('123');
-        this.socket.on('connect', () => {
-            console.log(this.socket.id + ' has joined');
-        });
-    }
+  constructor(private elementHelper: ElementHelperService) {
+    socketIo();
+    this.socket = socketIo();
+    console.log('123');
+    this.socket.on('connect', () => {
+      console.log(this.socket.id + ' has joined');
+    });
+  }
 
-    ngOnInit() {
-        $(document).on('input', event => {
-            let el = event.target,
-                elValue = $(el).val();
+  ngOnInit() {
+    $(document).on('input', event => {
+      let el = event.target,
+        elValue = $(el).val();
 
-            const location = this.elementHelper.elementLocation(el);
-            this.socket.emit('inputChanged', {
-                location,
-                value: elValue
-            });
-        });
+      const location = this.elementHelper.elementLocation(el);
+      this.socket.emit('inputChanged', {
+        location,
+        value: elValue
+      });
+    });
 
-        this.socket.on('onInputChanged', data => {
-            console.log(data);
-            const el = this.elementHelper.findElement(
-                data.inputData.location,
-                null
-            );
-            $(el).val(data.inputData.value);
-        });
-    }
+    this.socket.on('onInputChanged', data => {
+      console.log(data);
+      const el = this.elementHelper.findElement(data.inputData.location, null);
+      $(el).val(data.inputData.value);
+    });
+  }
 }
